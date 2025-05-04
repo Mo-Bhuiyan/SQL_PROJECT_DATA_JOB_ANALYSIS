518ef03fbf303508775ccb60127b274259b09e71
Mo-Bhuiyan <naim610@gmail.com>
2025-05-04 15:38:04 -0400

    Comma Join + WHERE Clause (Old Style) vs Left Join

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -45,3 +45,13 @@ WHERE
     AND job_location = 'Anywhere'
 ORDER BY salary_year_avg DESC
 LIMIT 10;
+
+-- above two queries are same results but different ways of joinined. The former is an old way, which is Comma Join + Where clause, which is basically the same as INNER JOIN
+-- However in the former I did left join, so how did the two produce same results? Because every job posting has a corresponding company ID. There are no rows with missing values.
+-- If there was, THEN they would yield different results. Here is proof none are missing.
+-- I am doing a left join and asking if there is a company.id = null for basically any job posting row. If below is = 0 then there is 0 rows in job posting facts table where company ID is null meaning inner join and left join on company ID will be same 
+
+SELECT COUNT(*) 
+FROM job_postings_fact jp
+LEFT JOIN company_dim cd ON jp.company_id = cd.company_id
+WHERE cd.company_id IS NULL;
